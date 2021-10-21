dfbcd49a270869b637739e12770ea010fb8495dc
Alicia Watt <alicia.watt.aw@gmail.com>
2021-10-21 17:21:00 -0600

    Model vehicle hash

---
+++ models/model_b.rb
@@ -0,0 +1,12 @@
+vehicle = {
+  "make" => 'Audi',
+  "model" => 'Q5',
+  "year_made" => 2018,
+  "miles_per_gallon" => 40.5,
+  "all_wheel_drive" => true,
+  "premium_gas" => true,
+  "interior" => ['black leather', 'digital dashboard', 'wood grain details'],
+  "exterior" => ['black paint', 'panoramic moon roof', 'led headlights']
+
+
+}

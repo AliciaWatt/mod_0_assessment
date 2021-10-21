1e17eff21f6dfc5b65bdf2ab9a1bfa5a1289581a
Alicia Watt <alicia.watt.aw@gmail.com>
2021-10-21 16:47:50 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,10 @@
+famous_person = "Neil deGrasse Tyson"
+is_active = true
+famous_person = "Carl Sagan"
+num_3 = 10
+num_9 = 5
+Sum = num9 + num_3
+puts "sum #{sum}"
+animals = ["Otter", "Kangaroo", "Goose"]
+a.drop ("Otter")
+puts famous_person.length

f0cd70e6d857c86c173f2c3d16e4d2c227760898
Alicia Watt <alicia.watt.aw@gmail.com>
2021-10-21 17:08:17 -0600

    Model airport hash

---
+++ models/model_a.rb
@@ -0,0 +1,12 @@
+Airport = {
+  "Name" => 'Doggo International Airport',
+  "nearest_town" => 'Denver, CO',
+  "nickname" => 'Dog Park of the Skies'
+  "number_of_runways" => 5,
+  "elevation" => 7500,
+  "international" => true,
+  "cats_allowed" => false,
+  "airlines" => ['Southwest', 'Lufthansa', 'United'],
+  "perks" => ['Squeaky Toys', 'Long Walks', 'Dog Park Acsess'],
+
+}
